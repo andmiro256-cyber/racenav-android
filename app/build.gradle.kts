ac73aa6c761842be418c389b91376eb8ab15cd7d
Andrey <andrey@trophy-nav.ru>
2026-03-09 18:23:34 +0000

    bump version to 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.andreykoff.racenav"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.7.0"
+        versionCode = 20
+        versionName = "1.7.1"
     }
 
     signingConfigs {
